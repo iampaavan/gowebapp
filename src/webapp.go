8d770f5816aa57c020fa08010b7d1b1bb99af1a0
iampaavan <gopalareddy.p@husky.neu.edu>
2020-03-29 23:15:06 -0400

    go implementation

--- src/webapp.go
+++ src/webapp.go
@@ -24,10 +24,33 @@ func GetTime(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(time)
+
+}
+
+func GetTimeZone(w http.ResponseWriter, r *http.Request){
+	
+	keys, ok := r.URL.Query()["key"]
+	
+	if !ok || len(keys[0]) < 1 {
+        log.Println("Url Param 'key' is missing")
+        return
+    }
+	
+	key := keys[0]
+	
+	log.Println("Url Param 'key' is: " + string(key))
+
+	loc, _ := time2.LoadLocation(key)
+    now := time2.Now().In(loc)
+    fmt.Println("ZONE : ", loc, " Time : ", now)
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(now)
 }
 
 func main(){
 	router := mux.NewRouter()
 	router.HandleFunc("/", GetTime).Methods("GET")
+	router.HandleFunc("/time", GetTimeZone).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8086", router))
 }
